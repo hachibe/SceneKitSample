ef0bfc2d22dc4a50af6dacc6f2d302eaf5f54ab8
hachibe <tsnorionori@gmail.com>
2017-06-27 02:51:19 +0900

    現在地の地図を平面に表示して俯瞰するよう変更

--- SceneKitSample/GameViewController.swift
+++ SceneKitSample/GameViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import MapKit
+import CoreLocation
 
 class GameViewController: UIViewController {
 
@@ -24,7 +26,8 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 10, z: 15)
+        cameraNode.rotation = SCNVector4(1, 0, 0, -0.2 * Float.pi)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -41,51 +44,17 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // create a cube node
-        let cubeGeometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
-        // create cube textures
-        let frontMaterial = SCNMaterial()
-//        frontMaterial.diffuse.contents = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
-        let font = UIFont.boldSystemFont(ofSize: 20)
-        let style = NSMutableParagraphStyle()
-        style.alignment = .center
-        style.lineBreakMode = .byClipping
-        let shadow = NSShadow()
-        shadow.shadowOffset = CGSize(width: 0.5, height: 0.5)
-        shadow.shadowColor = UIColor.darkGray
-        shadow.shadowBlurRadius = 0
-        let attributes = [
-            NSAttributedStringKey.font: font,
-            NSAttributedStringKey.paragraphStyle: style,
-            NSAttributedStringKey.shadow: shadow,
-            NSAttributedStringKey.foregroundColor: UIColor.black,
-            NSAttributedStringKey.backgroundColor: UIColor.white]
-        let frontString = "前" as NSString
-        let frontImage = frontString.image(with: attributes,
-                                           at: CGRect(origin: CGPoint.zero, size: CGSize(width: 20, height: 24)))
-        // 上下左右に黒い部分ができる
-//        let frontImage = frontString.image(with: attributes,
-//                                           at: CGRect(origin: CGPoint(x: 1, y: 2), size: CGSize(width: 28, height: 28)))
-        frontMaterial.diffuse.contents = frontImage
-        
-        let backMaterial = SCNMaterial()
-        backMaterial.diffuse.contents = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
-        let leftMaterial = SCNMaterial()
-        leftMaterial.diffuse.contents = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
-        let rightMaterial = SCNMaterial()
-        rightMaterial.diffuse.contents = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
-        let topMaterial = SCNMaterial()
-        topMaterial.diffuse.contents = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        let bottomMaterial = SCNMaterial()
-        bottomMaterial.diffuse.contents = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-        
-        cubeGeometry.materials = [frontMaterial, rightMaterial, backMaterial, leftMaterial, topMaterial, bottomMaterial]
-        
+        let cubeGeometry = self.cubeGeometry()
         let cubeNode = SCNNode(geometry: cubeGeometry)
-        cubeNode.position = SCNVector3(x: 0, y: 0, z: 0)
+        cubeNode.position = SCNVector3(x: 0, y: Float(cubeGeometry.height / 2), z: 0)
         scene.rootNode.addChildNode(cubeNode)
         
         // animate the 3d object
         cubeNode.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        
+        // create a map image plane node
+        setupLocation()
+        createMapImagePlane()
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -158,8 +127,106 @@ class GameViewController: UIViewController {
             return .all
         }
     }
+    
+    // MARK: - Cube
+    
+    private func cubeGeometry() -> SCNBox {
+        let cubeGeometry = SCNBox(width: 2.0, height: 2.0, length: 2.0, chamferRadius: 0.0)
+        // create cube textures
+        let frontMaterial = SCNMaterial()
+        let font = UIFont.boldSystemFont(ofSize: 20)
+        let style = NSMutableParagraphStyle()
+        style.alignment = .center
+        style.lineBreakMode = .byClipping
+        let shadow = NSShadow()
+        shadow.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        shadow.shadowColor = UIColor.darkGray
+        shadow.shadowBlurRadius = 0
+        let attributes = [
+            NSAttributedStringKey.font: font,
+            NSAttributedStringKey.paragraphStyle: style,
+            NSAttributedStringKey.shadow: shadow,
+            NSAttributedStringKey.foregroundColor: UIColor.black,
+            NSAttributedStringKey.backgroundColor: UIColor.white]
+        let frontString = "前" as NSString
+        let frontImage = frontString.image(with: attributes,
+                                           at: CGRect(origin: CGPoint.zero, size: CGSize(width: 20, height: 24)))
+        frontMaterial.diffuse.contents = frontImage
+        
+        let backMaterial = SCNMaterial()
+        backMaterial.diffuse.contents = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        let leftMaterial = SCNMaterial()
+        leftMaterial.diffuse.contents = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+        let rightMaterial = SCNMaterial()
+        rightMaterial.diffuse.contents = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        let topMaterial = SCNMaterial()
+        topMaterial.diffuse.contents = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        let bottomMaterial = SCNMaterial()
+        bottomMaterial.diffuse.contents = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        
+        cubeGeometry.materials = [frontMaterial, rightMaterial, backMaterial, leftMaterial, topMaterial, bottomMaterial]
+        return cubeGeometry
+    }
+    
+    // MARK: - Location
+    
+    private var locationManager: CLLocationManager?
+    
+    private func setupLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager?.delegate = self
+            locationManager?.startUpdatingLocation()
+        }
+    }
+    
+    // MARK: - Map
+    
+    private func createMapImagePlane() {
+        let options = MKMapSnapshotOptions()
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let centerCoordinate: CLLocationCoordinate2D
+        if let coordinate = locationManager?.location?.coordinate {
+            centerCoordinate = coordinate
+        } else {
+            centerCoordinate = CLLocationCoordinate2DMake(35.702069, 139.775327) // 東京駅
+        }
+        options.region = MKCoordinateRegionMake(centerCoordinate, span)
+        
+        let snapshotter = MKMapSnapshotter(options: options)
+        snapshotter.start(with: DispatchQueue.global(), completionHandler: { (snapshot, error) in
+            DispatchQueue.main.async {
+                guard let image = snapshot?.image, let scnView = self.view as? SCNView, let scene = scnView.scene else {
+                    return
+                }
+                let planeGeometry = SCNPlane(width: image.size.width/20, height: image.size.height/20)
+                let material = SCNMaterial()
+                material.diffuse.contents = image
+                planeGeometry.firstMaterial = material
+                let planeNode = SCNNode(geometry: planeGeometry)
+                planeNode.position = SCNVector3(x: 0, y: 0, z: 0)
+                planeNode.rotation = SCNVector4(1, 0, 0, -0.5 * Float.pi)
+                scene.rootNode.addChildNode(planeNode)
+            }
+        })
+    }
 }
 
+// MARK: - CLLocationManagerDelegate
+extension GameViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        }
+    }
+}
+
+// MARK: - NSString
 extension NSString {
     func image(with attributes: [NSAttributedStringKey: Any]? = nil, at rect: CGRect) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
